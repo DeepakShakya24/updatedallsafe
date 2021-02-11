70d03923ab04a190a1e64b121606bb5fa15e878d
Atulshakya <53398949+Atulshakya@users.noreply.github.com>
2021-02-11 19:09:07 +0530

    upated accounts

--- src/components/Accounts/Accounts.jsx
+++ src/components/Accounts/Accounts.jsx
@@ -4,38 +4,38 @@ import NavBar from "../NavBar/NavBar";
 //import Footer from "../Footer/Footer";
 const Accounts = () => {
   return (
-    <>
+    <div className="account-section">
       <NavBar />
       <div className="accounts">
         <div className="row">
           <div className="col-md-3">
             <div className="login">
-              <i className="fa fa-user fa-4x"></i>
+              <i className="fa fa-user fa-3x"></i>
               <h3>School Login</h3>
             </div>
           </div>
           <div className="col-md-3">
             <div className="login">
-              <i className="fa fa-user fa-4x"></i>
+              <i className="fa fa-user fa-3x"></i>
               <h3>User Login</h3>
             </div>
           </div>
           <div className="col-md-3">
             <div className="login">
-              <i className="fa fa-user fa-4x"></i>
+              <i className="fa fa-user fa-3x"></i>
               <h3>Instructor Login</h3>
             </div>
           </div>
           <div className="col-md-3">
             <div className="login">
-              <i className="fa fa-user fa-4x"></i>
+              <i className="fa fa-user fa-3x"></i>
               <h3>Vendor Login</h3>
             </div>
           </div>
         </div>
       </div>
       {/* <Footer /> */}
-    </>
+    </div>
   );
 };
 export default Accounts;
