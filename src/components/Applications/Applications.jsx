25b403c1860582874ea4d6edf7427e1eb1c49fec
Atulshakya <53398949+Atulshakya@users.noreply.github.com>
2021-03-08 13:19:08 +0530

    updated application

--- src/components/Applications/Applications.jsx
+++ src/components/Applications/Applications.jsx
@@ -245,7 +245,11 @@ function Applications() {
             </div>
           </div>
           <div className="col-md-4 layer-screen">
-            <img src={group3} alt="" style={{ width: "119%" }} />
+            <img
+              src={group3}
+              alt=""
+              style={{ width: "119%", height: "fit-content" }}
+            />
           </div>
         </div>
       </section>
